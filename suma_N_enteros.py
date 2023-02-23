af094fd93597ba70efae521506e1d000e7107fb8
JFEstevezC <jose2224646@correo.uis.edu.co>
2023-02-23 10:33:50 -0500

    Suma N enteros

---
+++ suma_N_enteros.py
@@ -0,0 +1,12 @@
+import math
+print("----------------------")
+print("----SUMA-N-ENTEROS----")
+print("----------------------")
+#Imput
+n =input("Digite el número de valores que quiere sumar ")
+n = int(n)
+#Processing
+suma = n*(n+1)/2
+#Output
+print("------------RESULTADOS------------")
+print("La suma total es: "+str(suma))
